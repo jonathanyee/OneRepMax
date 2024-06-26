84fee6ed72c5df695fb792020c34e7f302e6f9e5
Jonathan Yee <jonathanyee011@gmail.com>
2024-06-25 20:42:39 -0700

    project setup

---
+++ OneRepMax/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneRepMax
+//
+//  Created by Jonathan Yee on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneRepMax/OneRepMaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneRepMaxApp.swift
+//  OneRepMax
+//
+//  Created by Jonathan Yee on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct OneRepMaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
